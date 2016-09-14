8a507c9f7a2dca896529f047d3c6ef150691702e
Stefan <Stefan@STEFAN-PC>
2016-09-14 19:18:14 +0200

    Add project files.

---
+++ BVC.MVCDashboard.WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BVC.MVCDashboard.WebApplication.Startup))]
+namespace BVC.MVCDashboard.WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
